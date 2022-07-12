dd8a34805cff8eb6635d2ad3a5d974507fbe7a9e
Dilmurod03 <dahmadjonov1031@gmail.com>
2022-07-12 21:36:48 +0500

    book_shelter created

---
+++ js/login.js
@@ -0,0 +1,43 @@
+let elForm = document.querySelector(".form__login");
+let elUsernameInput = document.querySelector(".user-input");
+let elPasswordInput = document.querySelector(".password-input");
+let elError = document.querySelector(".error__div");
+let elLogin = document.querySelector(".submit__btn");
+
+elForm.addEventListener("submit", (evt) => {
+  evt.preventDefault();
+
+  let UsernameValue = elUsernameInput.value;
+  let PasswordValue = elPasswordInput.value;
+
+  fetch("https://reqres.in/api/login", {
+    method: "POST",
+    headers: {
+      "Content-Type": "application/json",
+    },
+    body: JSON.stringify({
+      email:  "eve.holt@reqres.in",
+      password: "cityslicka",
+    }),
+  })
+    .then((res) => res.json())
+    .then((data) => {
+      if (data?.token) {
+        window.localStorage.setItem("token", data.token);
+        window.location.replace("home.html");
+        elLogin.style.backgroundColor = "green";
+      } else {
+        elError.classList.remove("hidden");
+      }
+    });
+  elUsernameInput.value = "";
+  elPasswordInput.value = "";
+});
+elUsernameInput.addEventListener("keydown", function () {
+  elUsernameInput.style.Color = "black"
+  elLogin.style.backgroundColor = "white";
+});
+elPasswordInput.addEventListener("keydown", function () {
+    elUsernameInput.style.Color = "black"
+    elLogin.style.backgroundColor = "white";
+});
